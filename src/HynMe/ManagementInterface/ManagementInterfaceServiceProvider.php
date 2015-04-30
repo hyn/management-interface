7182bd551aadc11aefa66141a9af2873eaffb23d
Daniel Klabbers <dklabbers@hostingxs.nl>
2015-04-30 15:43:50 +0200

    first commit

---
+++ src/HynMe/ManagementInterface/ManagementInterfaceServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace HynMe\ManagementInterface;
+
+use Illuminate\Support\ServiceProvider;
+
+class ManagementInterfaceServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return [];
+	}
+
+}
