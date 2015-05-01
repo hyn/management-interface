9798025ac7aef70fef3149b2cdb8658ab581e240
Daniel Klabbers <dklabbers@hostingxs.nl>
2015-05-01 13:45:16 +0200

    fixed title and added dashboard controller

---
+++ src/HynMe/ManagementInterface/Controllers/DashboardController.php
@@ -0,0 +1,11 @@
+<?php namespace HynMe\ManagementInterface\Controllers;
+
+use HynMe\Framework\Controllers\AbstractController;
+
+class DashboardController extends AbstractController
+{
+    public function index()
+    {
+        
+    }
+}
