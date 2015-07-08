9e256ad19fae76cb2fc21b24a9cc99347c765f21
Daniel Klabbers <dklabbers@hostingxs.nl>
2015-07-08 23:33:16 +0200

    fixed the modals

--- src/views/website/forms/add-website.blade.php
+++ src/views/website/forms/add-website.blade.php
@@ -4,22 +4,24 @@
      @endif
     >
     <div class="modal-dialog">
-        <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-            <h4 class="modal-title" id="formModalLabel">{{ trans('management-interface::website.add-website-title') }}</h4>
-        </div>
         <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                <h4 class="modal-title" id="formModalLabel">{{ trans('management-interface::website.add-website-title') }}</h4>
+            </div>
+            <div class="modal-body">
 
-            {!! $form->open() !!}
-                <div class="modal-body">
-                    {!! $form->text('identifier', trans('management-interface::website.identifier'), ['placeholder' => trans('management-interface::website.identifier-ex')]) !!}
-                    {!! $form->ajaxSelect('tenant_id', trans_choice('management-interface::tenant.tenant',1), ['url' => route('management-interface@tenant@ajax')]) !!}
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">{{ trans('management-interface::action.cancel') }}</button>
-                    <button type="submit" class="btn btn-primary">{{ trans('management-interface::action.add') }}</button>
-                </div>
-            {!! $form->close() !!}
+                {!! $form->open() !!}
+                    <div class="modal-body">
+                        {!! $form->text('identifier', trans('management-interface::website.identifier'), ['placeholder' => trans('management-interface::website.identifier-ex')]) !!}
+                        {!! $form->ajaxSelect('tenant_id', trans_choice('management-interface::tenant.tenant',1), ['url' => route('management-interface@tenant@ajax')]) !!}
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">{{ trans('management-interface::action.cancel') }}</button>
+                        <button type="submit" class="btn btn-primary">{{ trans('management-interface::action.add') }}</button>
+                    </div>
+                {!! $form->close() !!}
+            </div>
         </div>
     </div>
 </div>
