b00ebaad238377bf9ab4e94ba773b3dbbc1f9822
Daniel Klabbers <daniel+hyn@klabbers.email>
2015-07-09 22:18:15 +0200

    added example service provider

---
+++ src/PackageServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace LaraLeague\Package;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class PackageServiceProvider
+ * @package LaraLeague\Package
+ */
+class PackageServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // TODO: Implement register() method.
+    }
+}
