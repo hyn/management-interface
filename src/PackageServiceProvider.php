1504ca4472df92c17727ebf232a389476ea1f48a
Daniel Klabbers <daniel+hyn@klabbers.email>
2015-07-09 22:36:32 +0200

    added routes and other registrations

--- src/PackageServiceProvider.php
+++ src/PackageServiceProvider.php
@@ -28,6 +28,10 @@ class PackageServiceProvider extends ServiceProvider
         $this->registerViews();
         $this->registerMigrations();
         $this->registerAssets();
+        $this->registerTranslations();
+        $this->registerConfigurations();
+
+        $this->registerRoutes();
     }
 
     /**
@@ -36,11 +40,11 @@ class PackageServiceProvider extends ServiceProvider
     protected function registerViews()
     {
         // register views within the application with the set namespace
-        $this->loadViewsFrom($this->packagePath('views'), 'package');
+        $this->loadViewsFrom($this->packagePath('assets/views'), 'package');
         // allow views to be published to the storage directory
         $this->publishes([
-            $this->packagePath('views') => base_path('resources/views/lara-league/package'),
-        ]);
+            $this->packagePath('assets/views') => base_path('resources/views/lara-league/package'),
+        ], 'views');
     }
 
     /**
@@ -48,7 +52,9 @@ class PackageServiceProvider extends ServiceProvider
      */
     protected function registerMigrations()
     {
-
+        $this->publishes([
+            $this->packagePath('assets/migrations') => database_path('/migrations')
+        ], 'migrations');
     }
 
     /**
@@ -56,11 +62,43 @@ class PackageServiceProvider extends ServiceProvider
      */
     protected function registerAssets()
     {
+        $this->publishes([
+            $this->packagePath('/assets/public') => public_path('lara-league/package'),
+        ], 'public');
+    }
 
+    /**
+     * Register the package translations
+     */
+    protected function registerTranslations()
+    {
+        $this->loadTranslationsFrom($this->packagePath('assets/translations'), 'package');
+    }
+
+    /**
+     * Register the package configurations
+     */
+    protected function registerConfigurations()
+    {
+        $this->mergeConfigFrom(
+            $this->packagePath('assets/configurations/config.php'), 'package'
+        );
+        $this->publishes([
+            $this->packagePath('assets/configurations/config.php') => config_path('package.php'),
+        ], 'config');
+    }
+
+    /**
+     * Register the package routes
+     */
+    protected function registerRoutes()
+    {
+        require_once {$this->packagePath('assets/routes.php')};
     }
 
     /**
      * Loads a path relative to the package base directory
+     *
      * @param string $path
      * @return string
      */
