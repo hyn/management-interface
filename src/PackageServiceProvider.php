37a787440609fc852eaac19b555ca33b5f9b3f51
Daniel Klabbers <daniel+hyn@klabbers.email>
2015-07-09 22:26:58 +0200

    added registration methods

--- src/PackageServiceProvider.php
+++ src/PackageServiceProvider.php
@@ -17,6 +17,55 @@ class PackageServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        // TODO: Implement register() method.
+    }
+
+    /**
+     * Application is booting
+     */
+    public function boot()
+    {
+
+        $this->registerViews();
+        $this->registerMigrations();
+        $this->registerAssets();
+    }
+
+    /**
+     * Register the package views
+     */
+    protected function registerViews()
+    {
+        // register views within the application with the set namespace
+        $this->loadViewsFrom($this->packagePath('views'), 'package');
+        // allow views to be published to the storage directory
+        $this->publishes([
+            $this->packagePath('views') => base_path('resources/views/lara-league/package'),
+        ]);
+    }
+
+    /**
+     * Register the package migrations
+     */
+    protected function registerMigrations()
+    {
+
+    }
+
+    /**
+     * Register the package public assets
+     */
+    protected function registerAssets()
+    {
+
+    }
+
+    /**
+     * Loads a path relative to the package base directory
+     * @param string $path
+     * @return string
+     */
+    protected function packagePath($path = '')
+    {
+        return sprintf("%s/%s", __DIR__ . '../', $path);
     }
 }
